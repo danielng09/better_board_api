1d7a0b7fe2348123c4a05aeeee49460bbff73c3e
Daniel Ng <dvng13@gmail.com>
2015-11-01 13:30:34 -0800

    troubleshoot rails console

--- db/migrate/20151031230342_create_users.rb
+++ db/migrate/20151031230342_create_users.rb
@@ -1,14 +1,18 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.string :provider
-      t.string :uid
-      t.string :name
-      t.string :email
-      t.string :oauth_token
+      t.string :provider, null: false
+      t.string :uid, null: false
+      t.string :name, null: false
+      t.string :email, null: false
+      t.string :oauth_token, null: false
       t.datetime :oauth_expires_at
 
       t.timestamps null: false
     end
+
+    add_index :users, :name
+    add_index :users, :email, unique: true
+    add_index :users, :oauth_token
   end
 end
