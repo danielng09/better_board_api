dbcec82fd0a747ba5d0a350aad7def06ffb8bbb2
Daniel Ng <dvng13@gmail.com>
2015-11-06 12:17:18 -0800

    modify job posting serializer

--- app/serializers/job_posting_serializer.rb
+++ app/serializers/job_posting_serializer.rb
@@ -1,3 +1,7 @@
 class JobPostingSerializer < ActiveModel::Serializer
-  attributes :title, :company, :source, :location, :date_posted, :description
+  attributes :title, :company, :source, :location, :date_posted, :description, :url
+
+  def date_posted
+    object.date_posted.strftime("%m/%d/%Y")
+  end
 end
