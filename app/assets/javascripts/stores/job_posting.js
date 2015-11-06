38e6de151d0553778fb00393a81eb8a66cf40345
Daniel Ng <dvng13@gmail.com>
2015-11-06 12:14:23 -0800

    remove unnecessary views and assets

--- app/assets/javascripts/stores/job_posting.js
+++
@@ -1,8 +0,0 @@
-(function(root){
-  var _job_postings = [];
-  root.BenchStore = $.extend({}, EventEmitter.prototype, {
-    all: function () {
-      return _benches.slice(0);
-    }
-  })
-})(this);
--- app/views/api/job_postings/show.json.jbuilder
+++
@@ -1,1 +0,0 @@
-json.extract! @job_posting, :source_id, :title, :company, :date_posted, :description, :location, :description, :url
