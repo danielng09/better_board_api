84be790d30182f12aff03deceee00a723dd9ab9e
Daniel Ng <dvng13@gmail.com>
2015-11-18 19:43:46 -0800

    change job postings controller

--- app/controllers/api/job_postings_controller.rb
+++ app/controllers/api/job_postings_controller.rb
@@ -5,15 +5,20 @@ class API::JobPostingsController < ApplicationController
     qs = job_posting_params[:q].empty? ? {} : { _all: job_posting_params[:q] }
     response = JobPosting.search(qs)
     @job_postings = response.page(job_posting_params[:page]).as_json
+
+    #move page info logic into model layer
     postings_total = response.response.hits.total
     max_postings_shown = JobPosting.per_page * job_posting_params[:page].to_i
+    div, mod = postings_total.divmod(JobPosting.per_page)
+    total_pages = mod == 0 ? div : div + 1
     postings_shown = max_postings_shown > postings_total ? postings_total : max_postings_shown
 
     render json: @job_postings,
            each_serializer: JobPostingsSerializer,
            meta: { postings_total: postings_total,
                    postings_shown: postings_shown,
-                   page: job_posting_params[:page] }
+                   page: job_posting_params[:page],
+                   total_pages: total_pages}
   end
 
   def show
