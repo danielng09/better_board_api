ebc43407b8ba5bd0f31485d96d3ad09f2c0b2d1e
Daniel Ng <dvng13@gmail.com>
2015-11-19 21:39:34 -0800

    heroku

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,6 +1,1 @@
 JobPosting.__elasticsearch__.client = Elasticsearch::Client.new url: ENV['BONSAI_URL']
-
-# unless JobPosting.__elasticsearch__.index_exists?
-#   JobPosting.__elaticsearch__.create_index! force: true
-#   JobPosting.import
-# end
